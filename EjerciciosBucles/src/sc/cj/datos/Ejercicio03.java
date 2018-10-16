5155a1b91685f6cd1be6447b1abcd112d87b87c5
Alumno <Alumno@AULA29-03>
2018-10-16 14:11:09 +0200

    ejercicios bucles 2

--- EjerciciosBucles/src/sc/cj/datos/Ejercicio03.java
+++ EjerciciosBucles/src/sc/cj/datos/Ejercicio03.java
@@ -9,10 +9,17 @@ public class Ejercicio03 {
 	public static void main(String[] args) {
 		//Programa que muestre y calcule el producto de los n primeros números impares dentro de un numero (ejemplo: pone el 5, que multiplique lso 5 primeros numeros impares, en nuestro caso seria 1, 3, 5, 7, 9)
 		
-		int cantidad = 0, contadorimpar = 0;
-		double numero = 0;
+		int cantidad = 0, contadorimpar = 1;
 		
-		
-
+		System.out.println("introduzca un valor");
+		cantidad = SC.nextInt();
+		for (int i = 0; i <= cantidad; i++) {
+			int uno = (i % 2);
+			if (uno != 0) {
+				contadorimpar = i * contadorimpar;
+				uno = 0;
+			}
+		}
+		System.out.println(contadorimpar);
 	}
 }
