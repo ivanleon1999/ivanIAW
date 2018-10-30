70737d714167c76f26f80d9289ec111ed73b6521
Alumno <Alumno@AULA29-03>
2018-10-30 14:41:10 +0100

    nuevo ejercicio

--- EjerciciosTablas/src/es/cj/ejerciciciostablas/datos/Ejercicio06.java
+++ EjerciciosTablas/src/es/cj/ejerciciciostablas/datos/Ejercicio06.java
@@ -5,56 +5,56 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Ejercicio06 {
-	
+
+	// Crear una tabla de n elementos. Pedir al usuario un valor entre 0 y n-1.
+	// Eliminar el valor de esa posición y desplazar todos los elementos para no
+	// dejar el hueco vacío
+
 	private static Scanner sc = new Scanner(System.in);
-	private static int [] numeros;
 	
-	
+	private static int [] tabla;
 	
 	public static void main(String[] args) {
-		//Crear una tabla de n números y desplazarla una posición hacia abajo, es decir, el primero pasa a ser el segundo, el segundo pasa a ser el tercero y así sucesivamente. El último pasa a ser el primero
-
-		int tamanyo;
+		int tamanyo = 0;
 		do {
-			System.out.println("nº de elementos");
+			System.out.println("Nº Elementos: ");
 			tamanyo = sc.nextInt();
-		} while (tamanyo <=0);
-	
-		numeros = new int [tamanyo];
+		} while (tamanyo <= 0);
 		
-		iniciar();
+		tabla = new int[tamanyo];
 		
-		mostrar();
+		inicializarTabla();
 		
-		desplazar();
+		mostrarTabla();
 		
-		mostrar();
-
-	sc.close();
+		int posicion;
+		do {
+			System.out.println("Posición: ");
+			posicion = sc.nextInt();
+		} while (posicion < 0 || posicion > tamanyo);
+		
+		eliminarPosicion(posicion);
+		
+		mostrarTabla();
+		
+		sc.close();
 	}
 
-
-
-	private static void desplazar() {
-		int aux = numeros[numeros.length -1];
-		for (int i = numeros.length - 1; i > 0; i--) {
-			numeros[i]=numeros[i-1];
+	private static void eliminarPosicion(int posicion) {
+		for (int i = posicion; i < tabla.length - 1; i++) {
+			tabla[i] = tabla[i+1];
 		}
-		numeros[0]=aux;
+		tabla[tabla.length - 1] = 0;
 	}
 
-
-	private static void mostrar() {
-		System.out.println(Arrays.toString(numeros));
-		
+	private static void mostrarTabla() {
+		System.out.println(Arrays.toString(tabla));
 	}
 
-
-	private static void iniciar() {
-		
-		for (int i = 0; i < numeros.length; i++) {
-			numeros[i] = new Random().nextInt(50)+1;
+	private static void inicializarTabla() {
+		for (int i = 0; i < tabla.length; i++) {
+			tabla[i] = new Random().nextInt(50) + 1;
 		}
-		
 	}
+
 }
